0907bd2429d0232028e049a114ac93ead56412c9
Harshal <gangurdeharsh08@gmail.com>
2020-06-20 19:50:27 +0530

    set method to change favorite status of product

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,18 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-import '../models/product.dart';
+import '../providers/single_product_provider.dart';
 
 import '../screens/product_details_screen.dart';
 
 class ProductItem extends StatelessWidget {
-  final Product product;
-
-  ProductItem({
-    @required this.product,
-  });
-
   @override
   Widget build(BuildContext context) {
+    final product = Provider.of<SingleProductProvider>(context);
+
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
@@ -36,10 +33,12 @@ class ProductItem extends StatelessWidget {
           backgroundColor: Colors.black45,
           leading: IconButton(
             icon: Icon(
-              Icons.favorite,
-              color: Colors.white70,
+              product.isFavorite ? Icons.favorite : Icons.favorite_border,
+              color: Theme.of(context).accentColor,
             ),
-            onPressed: null,
+            onPressed: () {
+              product.toggleFavorite();
+            },
           ),
           trailing: IconButton(
             icon: Icon(
